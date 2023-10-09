2072492a1c71cf19d11e2e79f0bd4efb32f7cb39
akreminsky <akreminsky2023@yandex.ru>
2023-10-09 20:53:35 +0300

    All tasks completed

--- files/files.py
+++ files/files.py
@@ -2,6 +2,13 @@ import os
 import random
 
 
+class Type:
+    def __init__(self, work, age, height):
+        self.__work = work
+        self.__age = age
+        self.__height = height
+
+
 def files_creator():
     for i in range(10):
         if f'{i + 1}.txt' in os.listdir():
@@ -19,5 +26,14 @@ def sum_files(one, two):
         for s in file:
             sum_var += int(s.rstrip())
 
+
 files_creator()
 sum_files(random.randint(1, 10), random.randint(1, 10))
+sum_files(1, 2)
+
+
+def type_maker(file_name):
+    with open(file_name) as file:
+        for i in file:
+            args = i.split(" ")
+            _ = Type(args[0], args[1], args[2])
