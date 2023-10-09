970b21615c64c918f35545b25f50e947ebd4a358
akreminsky <akreminsky2023@yandex.ru>
2023-10-09 20:41:25 +0300

    Summator added

--- files/files.py
+++ files/files.py
@@ -1,11 +1,23 @@
+import os
 import random
 
 
 def files_creator():
     for i in range(10):
-        with open(f'{i + 1}.txt', "wt") as file:
-            for _ in range(3):
-                file.write(f'{random.randint(0, 10)} \n')
+        if f'{i + 1}.txt' in os.listdir():
+            with open(f'{i + 1}.txt', "wt") as file:
+                for _ in range(3):
+                    file.write(f'{random.randint(0, 10)} \n')
+
+
+def sum_files(one, two):
+    sum_var = 0
+    with open(f'{one}.txt') as file:
+        for s in file:
+            sum_var += s.rstrip()
+    with open(f'{two}.txt') as file:
+        for s in file:
+            sum_var += s.rstrip()
 
 
 files_creator()
