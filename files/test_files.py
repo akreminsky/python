b5014099c7a6854dff4539414233d233103ade2f
akreminsky <akreminsky2023@yandex.ru>
2023-10-10 20:14:48 +0300

    Обработка исключительных ситуаций

--- files/test_files.py
+++ files/test_files.py
@@ -1,7 +1,17 @@
 import unittest
 
+from files import sum_files_six_nums
+
 
 class FilesTests(unittest.TestCase):
 
     def test_regression(self):
-        self.assertEqual(sort_arr([0, 1, 2, 3, 0, 1, 2, 0]), [0, 0, 0, 1, 1, 2, 2, 3])
+        with open("new_file.txt", 'wt') as file1:
+            file1.write("3\n")
+            file1.write("5\n")
+            file1.write("9\n")
+        with open("new_file_1.txt", 'wt') as file2:
+            file2.write("2\n")
+            file2.write("6\n")
+            file2.write("190\n")
+        self.assertTrue(sum_files_six_nums(file1, file2) == 215)
