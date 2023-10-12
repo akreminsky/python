eb0226fed0ba2eb55ae060e087d92801249d0d17
akreminsky <akreminsky2023@yandex.ru>
2023-10-13 00:42:32 +0300

    Deleter

---
+++ Archive/archive.py
@@ -0,0 +1,12 @@
+import os
+from glob import glob
+from zipfile import ZipFile
+
+
+def scan_add(name, ext):
+    with ZipFile(name, 'w') as zip:
+        for i in glob(os.path.join(os.getcwd(), f'*{ext}')):
+            zip.write(i)
+
+
+print(scan_add('arc', '.htg'))
