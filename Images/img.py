20ae0ad504ffc14378946d7c27de390363c2a628
akreminsky <akreminsky2023@yandex.ru>
2023-10-15 23:10:37 +0300

    Images added

---
+++ Images/img.py
@@ -0,0 +1,22 @@
+import os
+
+from PIL import Image, ImageDraw
+
+
+def rect_drawer(image):
+    draw = ImageDraw.Draw(image)
+    rect = image.width / 10
+    shape = (image.width / 2 - rect, image.height / 2 - rect, image.width / 2 + rect, image.height / 2 + rect)
+    draw.rectangle(shape)
+    draw.multiline_text((image.width / 2 - rect + 2, image.height / 2), 'Hello\nWorld')
+    return image
+
+
+def converter(ext_in, ext_out):
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(ext_in):
+            image = Image.open(file)
+            rect_drawer(image).save(f'{os.path.splitext(file)[0]}{ext_out}')
+
+
+converter(".jpeg", ".png")
